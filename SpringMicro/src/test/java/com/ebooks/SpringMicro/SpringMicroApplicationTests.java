3172ae0ff00150d0210b34483b4b7aa631009919
Vishrut Agrawal <vishrutagrawal9@gmail.com>
2022-05-26 15:39:10 +0530

    initial commit

---
+++ SpringMicro/src/test/java/com/ebooks/SpringMicro/SpringMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebooks.SpringMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
